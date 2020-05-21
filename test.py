9270274126aee41111565630bc6e83acab0d5f91
rayray <ray85625@gmail.com>
2020-05-21 16:51:57 +0800

    cool

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-print "Hello Everyone"
-print "okay"
-print 1+100
+import sys
+import logging
+
+def main(argv=None):
+
+    log = logging.getLogger('test')
+    log.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter("%(message)s"))
+    log.addHandler(console_handler)
+
+    log.info('Starting something...')
+    log.info('Doing something...')
+    log.info('Finished something.')
+
+    logging.shutdown()
+
+if __name__=='__main__':
+    sys.exit(main(sys.argv[1:]))
