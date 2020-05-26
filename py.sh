95e31b9714dcc91b8636fcfbc25bde98f881210c
rayray <ray85625@gmail.com>
2020-05-26 16:34:22 +0800

    tt

--- py.sh
+++ py.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-git add -u
-git commit -m 'tt'
-git push
+ESLINT_ERR=$(eslint --quiet --no-eslintrc tj.js)
+echo ${ESLINT_ERR}
+if  [ -z "${ESLINT_ERR}" ]
+then
+    echo no_error
+else
+    echo gg
+fi
