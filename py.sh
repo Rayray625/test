940c3759cd1a0bc89a95017c9d642dad333541f9
rayray <ray85625@gmail.com>
2020-05-26 18:05:42 +0800

    tt

--- py.sh
+++ py.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-ESLINT_ERR=$(eslint --quiet --no-eslintrc tj.js)
-echo ${ESLINT_ERR}
-if  [ -z "${ESLINT_ERR}" ]
-then
-    echo no_error
-else
-    echo gg
-fi
+DIFF_FILES=$(git diff --name-only origin/master)
+echo $DIFF_FILES
+DIFF_PY_FILES=""
+for FILE_NAME in $DIFF_FILES; do
+    if [[ $FILE_NAME == *.py ]];
+    then
+        DIFF_PY_FILES="${DIFF_PY_FILES} ${FILE_NAME}"
+    fi
+    if [[ $FILE_NAME == *.js ]];
+    then
+        DIFF_JS_FILES="${DIFF_JS_FILES} ${FILE_NAME}"
+    fi
+done
+echo py=${DIFF_PY_FILES}
+echo js=${DIFF_JS_FILES}
